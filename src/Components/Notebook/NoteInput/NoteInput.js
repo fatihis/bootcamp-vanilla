e063a0720f3174ddc44927f79159b88a4e62d688
fatihis99@hotmail.com <isk.fatih1@gmail.com>
2021-09-11 15:49:57 +0300

    Add NoteInput

---
+++ src/Components/Notebook/NoteInput/NoteInput.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { useEffect, useState } from "react";
+import "./NoteInput.css";
+
+export default function NoteInput(props) {
+  const [inputText, setInputText] = useState("");
+
+  const sendData = () => {
+    props.addNewNote(inputText);
+  };
+
+  return (
+    <>
+      <input
+        type="text"
+        className="input"
+        onChange={(e) => setInputText(e.target.value)}
+      />
+      <button className="input-button" onClick={() => sendData()}>
+        ADD
+      </button>
+    </>
+  );
+}
