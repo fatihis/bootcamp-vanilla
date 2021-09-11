990f86888c25756add17d8cdf50fd2b20d8e4bf8
fatihis99@hotmail.com <isk.fatih1@gmail.com>
2021-09-11 12:01:54 +0300

    Initialize Project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
+import { TableRow } from "./Components/TableRow/TableRow";
+import data from "./data";
+import "./App.css";
+
 const App = () => {
   return (
     <div className="App">
-      App
+      <div className="home-table-container">
+        {data.currencies.map((element) => {
+          return (
+            <TableRow
+              exchangeType={element.exchangeType}
+              buy={element.buy}
+              sell={element.sell}
+              diff={element.diff}
+              exchangeFrom={element.exchangeFrom}
+              exchangeTo={element.exchangeTo}
+            ></TableRow>
+          );
+        })}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
