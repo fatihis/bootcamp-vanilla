40fd00aa953d99017593abd3386c97ef3eae3066
fatihis99@hotmail.com <isk.fatih1@gmail.com>
2021-09-11 12:02:07 +0300

    Add TableRow

---
+++ src/Components/TableRow/TableRow.js
@@ -0,0 +1,16 @@
+import React from "react";
+import TableCell from "../TableCell/TableCell";
+import "./TableRow.css";
+export const TableRow = (props) => {
+  console.log("invoked " + props);
+  return (
+    <div className="table-row-container">
+      <TableCell text={props.exchangeType} />
+      <TableCell text={props.exchangeFrom} />
+      <TableCell text={props.exchangeTo} />
+      <TableCell text={props.buy} />
+      <TableCell text={props.sell} />
+      <TableCell text={props.diff} />
+    </div>
+  );
+};
